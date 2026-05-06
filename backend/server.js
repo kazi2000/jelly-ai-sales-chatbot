aba0aaef374458f2acf62c5fe5417c47a554989b
kazi2000 <49372411+kazi2000@users.noreply.github.com>
2026-05-06 13:08:01 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import chatRoutes from "./routes/chat.js";
 import planRoutes from "./routes/plan.js";
 import usageRoutes from "./routes/usage.js";
+import storeRoutes from "./routes/store.js";
 
 dotenv.config();
 
 const app = express();
+
 app.use(express.json());
 
 app.use("/api", chatRoutes);
 app.use("/api", planRoutes);
 app.use("/api/usage", usageRoutes);
+app.use("/api", storeRoutes);
 
 app.get("/", (req, res) => {
   res.send("Jelly AI Backend Running");
